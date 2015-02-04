d4d7bf1e7ec026bbcc3c007a5446214cc685463e
Hoang Phuong <phuong1492.bka@gmail.com>
2015-02-04 16:35:33 +0700

    AdminEditUser

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -10,6 +10,20 @@ class Admin::UsersController < ApplicationController
     @user = User.find params[:id]
   end
 
+  def edit
+    @user = User.find params[:id]
+  end
+
+  def update
+    @user = User.find params[:id]
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to admin_root_path
+    else
+      render 'edit'
+    end
+  end
+
   private
   def admin_user
     redirect_to root_url unless current_user.supervisor?
