d5786437b0baaf3750e91a02d1537a323e9b446f
Hoang Phuong <phuong1492.bka@gmail.com>
2015-02-06 11:35:46 +0700

    Delete User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
 class User < ActiveRecord::Base
   attr_accessor :remember_token
-  before_save { self.email = email.downcase }
-  validates :name,  presence: true, length: { maximum: 50 }
+  before_save {self.email = email.downcase}
+  validates :name,  presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-    format: { with: VALID_EMAIL_REGEX },
-    uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: {maximum: 255},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, length: { minimum: 6 },allow_blank: true
+  validates :password, length: {minimum: 6},allow_blank: true
+
+  scope :without_user, ->user {where.not(id: user)}
   
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
