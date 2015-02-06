27e2ce6915abdd2609ff970b208a4c873b11c40b
Hoang Phuong <phuong1492.bka@gmail.com>
2015-02-06 08:14:04 +0700

    Admin add new User

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -6,6 +6,20 @@ class Admin::UsersController < ApplicationController
     @users = User.all
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Created " + @user.name + " !"
+      redirect_to admin_users_path
+    else
+      render 'new'
+    end
+  end
+
   def show
     @user = User.find params[:id]
   end
